a72cf175d952a68c96967e8f5cc9e9abcb2ef4b9
Nicole <52045376+drfiveminusmint@users.noreply.github.com>
2026-06-26 14:43:03 -0400

    emergency fix

--- src/main/java/com/github/drfiveminusmint/brickball/listener/PlayerListener.java
+++ src/main/java/com/github/drfiveminusmint/brickball/listener/PlayerListener.java
@@ -34,7 +34,7 @@ public class PlayerListener implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         BrickballMatch playerMatch = Brickball.getInstance().getMatchManager().getMatchByPlayer(player);
-        if (playerMatch == null || !player.getGameMode().equals(GameMode.ADVENTURE)) return;
+        if (playerMatch == null || playermatch.getState() != MatchState.RUNNING || !player.getGameMode().equals(GameMode.ADVENTURE)) return;
         playerMatch.checkDeathRegions(player);
         if (player.getInventory().contains(Material.BRICK))
             playerMatch.checkScoring(player);
